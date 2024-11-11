e3b8a2c83ceca1ac2943d85c74893a97c055d8af
Fabian Mora Robles <fabianmorar1223@gmail.com>
2024-11-11 14:23:40 -0600

    changes

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(cors({
   allowedHeaders: 'Content-Type,Authorization'
 }));
 
-app.use(express.json());
+app.use(express.json({limit: '5mb'}));
 
 // Ruta para generar el reporte de accesibilidad
 app.post('/generate-accessibility-report', async (req, res) => {
